0908fa991046bbd6272facc365211eb0c7a29e9a
Ícaro de Carvalho <decarvalho.icaro@gmail.com>
2021-10-19 10:06:44 -0300

    Seeds can now populate database
    Using seeds file to populate database locally may facilitate and speed up development.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+List.destroy_all
+Item.destroy_all
+
+rand(5..10).times do
+  list = List.create title: Faker::Movie.title
+
+  rand(10).times do
+    Item.create body: Faker::Movie.quote, list: list
+  end
+end
